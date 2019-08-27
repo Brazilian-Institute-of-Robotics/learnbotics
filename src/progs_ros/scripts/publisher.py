cce5a501a3c8d3f85f059b315eee99f0eaeea106
Leandro Nozela <nozela.leo@gmail.com>
2019-08-27 15:59:14 -0300

    Publisher.py script

---
+++ src/progs_ros/scripts/publisher.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def publisher():
+    rospy.init_node('talker', anonymous=True)
+
+    pub = rospy.Publisher('chatter', String, queue_size=10)
+    
+    rate = rospy.Rate(10) # 10hz
+
+    while not rospy.is_shutdown():
+        i = 0
+        hello_str = "hello world_" + str(i)
+        rospy.loginfo(hello_str)
+        pub.publish(hello_str)
+        rate.sleep()
+        i = i+1
+
+if __name__ == '__main__':
+    try:
+        publisher()
+    except rospy.ROSInterruptException:
+        pass
